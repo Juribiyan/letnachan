f6ec38991443e065d3425d6a1e8204348eb0e3eb
Juribiyan <smugleaf@ya.ru>
2023-09-04 23:18:36 +0500

    Prevent voting from hidden networks

--- lenta/html/rate.php
+++ lenta/html/rate.php
@@ -4,40 +4,47 @@ function rape($id, $method)
 {
     global $db; 
 
-    $res = $db->query("SELECT * FROM `blog` WHERE `id`='" . $id . "' AND `type`='thread'")
-    ->fetch_assoc();
-    $ip          = ip2long($_SERVER['REMOTE_ADDR']);
-    $get_numbers = $res['rating'];
-    //----
-    $sql_two     = "SELECT * FROM `rate` WHERE `ip`='" . $ip . "' AND `thread`='" . $id . "'";
-    $existing   = $db->query("SELECT COUNT(*) as cnt FROM `rate` WHERE `ip`='" . $ip . "' AND `thread`='" . $id . "'")
-    ->fetch_assoc()['cnt'];
-    if ($existing) {
-        $rating        = $db->query("SELECT * FROM `blog` WHERE `id`='" . $id . "'")->fetch_assoc();
-        $say['response'] = '100';
-        $say['message']  = 'Вы уже голосовали за эту новость';
-        exit(json_encode($say));
+    $ip = ip2long($_SERVER['REMOTE_ADDR']);
+
+    if($ip) {
+        $res = $db->query("SELECT * FROM `blog` WHERE `id`='" . $id . "' AND `type`='thread'")
+        ->fetch_assoc();
+        $get_numbers = $res['rating'];
+
+        $sql_two     = "SELECT * FROM `rate` WHERE `ip`='" . $ip . "' AND `thread`='" . $id . "'";
+        $existing   = $db->query("SELECT COUNT(*) as cnt FROM `rate` WHERE `ip`='" . $ip . "' AND `thread`='" . $id . "'")
+        ->fetch_assoc()['cnt'];
+        if ($existing) {
+            $rating        = $db->query("SELECT * FROM `blog` WHERE `id`='" . $id . "'")->fetch_assoc();
+            $say['response'] = '100';
+            $say['message']  = 'Вы уже голосовали за эту новость';
+            exit(json_encode($say));
+        }
+        //----
+        if ($method == 'up') {
+            $new_numbers = $get_numbers + 1;
+        }
+        if ($method == 'down') {
+            $new_numbers = $get_numbers - 1;
+        }
+        if ($get_numbers !== $new_numbers) {
+            $db->query("UPDATE `blog` SET `rating`='" . $new_numbers . "'  WHERE `id`='" . $id . "'");
+            $db->query("INSERT INTO `rate` SET `ip`='" . $ip . "',`thread`='" . $id . "'");
+            $say['response'] = '200';
+            $say['message']  = 'Ваш голос засчитан';
+            // -------------- Broadcast time! --------------
+            clientBroadcast("stats:$id", 'rating-update', [
+                'id' => $id,
+                'rating' => $new_numbers
+            ]);
+            exit(json_encode($say));
+        } else {
+            $say['message'] = 'Неизвестная ошибка';
+            die(json_encode($say));
+        }
     }
-    //----
-    if ($method == 'up') {
-        $new_numbers = $get_numbers + 1;
-    }
-    if ($method == 'down') {
-        $new_numbers = $get_numbers - 1;
-    }
-    if ($get_numbers !== $new_numbers) {
-        $db->query("UPDATE `blog` SET `rating`='" . $new_numbers . "'  WHERE `id`='" . $id . "'");
-        $db->query("INSERT INTO `rate` SET `ip`='" . $ip . "',`thread`='" . $id . "'");
-        $say['response'] = '200';
-        $say['message']  = 'Ваш голос засчитан';
-        // -------------- Broadcast time! --------------
-        clientBroadcast("stats:$id", 'rating-update', [
-            'id' => $id,
-            'rating' => $new_numbers
-        ]);
-        exit(json_encode($say));
-    } else {
-        $say['message'] = 'Неизвестная ошибка';
+    else {
+        $say['message'] = 'Вы не можете голосовать с этого IP';
         die(json_encode($say));
     }
 }
